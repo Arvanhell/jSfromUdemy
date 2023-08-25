9b3426d1179ced978a07820879ff9707c4a0d658
Arvanhell <arrivald@gmail.com>
2023-08-25 13:34:57 +0100

    added and updated view and bookmarks

--- 18-forkify/starter/src/js/views/bookmarksView.js
+++ 18-forkify/starter/src/js/views/bookmarksView.js
@@ -1,36 +1,16 @@
 import  View from './View.js';
+import previewView from './previewView.js';
 import icons from 'url:../../img/icons.svg'; // Parcel 2
 
 class BookmarksView extends View {
     _parentElement = document.querySelector('.bookmarks__list'); 
-    _errorMessage = 'No bookmarks yet. FInd a nice recipe and bookmarked ';
+    _errorMessage = 'No bookmarks yet. FInd a nice recipe and bookmark it 😎 ';
     _message = '';
 
     _generateMarkup() {
-        console.log(this._data);
-        return this._data.map(this._generateMarkupPreview).join('');
-
-    }
-
-    _generateMarkupPreview(result) {
-      const id = window.location.hash.slice(1);
-
-        return `
-        <li class="preview">
-            <a class="preview__link ${
-              result.id === id ? 'preview__link--active' : ''
-          }" href="#${result.id}">
-              <figure class="preview__fig">
-                <img src="${result.image}" alt="${result.title}" />
-              </figure>
-              <div class="preview__data">
-                <h4 class="preview__title">${result.title}</h4>
-                <p class="preview__publisher">${result.publisher}</p>
-                </div>
-              </div>
-            </a>
-          </li>
-        `;
+        return this._data
+        .map( bookmark => previewView.render(bookmark, false))
+        .join('');
     }
 }
 

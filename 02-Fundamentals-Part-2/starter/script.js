fb49ac94014148e6d173a0dbd3ba37aa1f9fede0
Arvanhell <arrivald@gmail.com>
2023-08-31 20:12:43 +0100

    added few line basic code

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,12 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10)
+
+console.log('Cezary');
+console.log(23);
+
+let firstName = 'Jolanta';
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+

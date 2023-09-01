3f9134129b8f421c54848b3cdb6fcdf776d6a34a
Arvanhell <arrivald@gmail.com>
2023-09-01 17:05:48 +0100

    new line added few new concept learned

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,3 +1,4 @@
+/*
 let js = "amazing";
 console.log(40 + 8 + 23 - 10)
 
@@ -9,4 +10,51 @@ let firstName = 'Jolanta';
 console.log(firstName);
 console.log(firstName);
 console.log(firstName);
+
+let cezar_jola = 'cj';
+let $function = 27;
+let PI = 3.1415;
+
+let myFirstJob = 'butcher';
+let myCurentJob = 'security';
+ // keep in mind about naming variables
+console.log(myFirstJob);
+let country = "Poland";
+let continent = 'Europe';
+let population = 37.776000 ;
+let language = 'polish'
+ 
+console.log(`I am from ${country} in ${continent} where live ${population} people, native language is ${language} `)
+
+const isIsland = "false"
+language = 'english'
+console.log('isIsland', 'population', 'country','language')
+
+console.log(population);
+
+let decsription = 'Portugal is in Europe, and its 11 milion people speak portuguease'
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof true);
+
+javascriptIsFun = 'Yes!';
+
+year = 1223;
+console.log(year);
+console.log(null);
+*/
+
+let age = 30;
+age = 41;
+console.log(age);
+const birthYear = 1991;
+// birthYear = 1990;
+// const job;
+
+//remember use always const ... unleas not needed change const const const
+
+
+
+
 

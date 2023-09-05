e3631dbc0aad0345f123fa291ba3ef7654560620
Arvanhell <arrivald@gmail.com>
2023-09-05 21:32:52 +0100

    Learned more about function declaration" wq "

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -54,10 +54,45 @@
 //              33. Function
 ///////////////////////////////////////////////////////////////
 //
- function logger () {
-    console.log('My name is Cezary');
- } 
- // calling / running / invoking / function
-logger();
-logger();
-logger();
+//              FUNCTION DECLARATION
+//
+//
+//    function logger () {
+//        console.log('My name is Cezary');
+//    } 
+//              calling / running / invoking / function  
+//    logger();
+//    logger();
+//    logger();
+//
+/////////////////////////
+//
+//    function fruitProcessor(apples, oranges) {
+//      
+//       const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+//        return juice;
+//      };
+//
+//          const appleJuice = fruitProcessor(5, 0); 
+//         //  Juice with 5 apples and 0 oranges // output in returned and saved const juice !!!!!!!!
+//
+////////////////////////
+//
+//    // we captured and saved value of produced juice within variable 
+//    // we created after return. Either returned value will be POoF!!!  and gone if not stored.
+//
+////////////////////////
+//    
+//    console.log(appleJuice);
+//              here we can call and see that appleJuice is stored in variable
+//    const appleOrangeJuice = fruitProcessor(2, 4);
+//    console.log(appleOrangeJuice);
+//          opt --> 2 4  
+//          opt --> Juice with 2 apples and 4 oranges  
+//    without executing the function there will be no output after we call 
+//
+///////////////////////////////////////////////////////////////
+//                  35. Function Declaration Vs Expressions
+///////////////////////////////////////////////////////////////
+   
+    

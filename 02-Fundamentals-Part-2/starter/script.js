c0d2b07ab5e39a84d6a48ca751a45242907f250e
Arvanhell <arrivald@gmail.com>
2023-09-07 19:38:45 +0100

    starting challenge

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -257,19 +257,20 @@
 //
   
 const calcAge = function(birthYear){
-    return 2037 - birthYear 
+    return 2023 - birthYear 
 }
 const yearsUntillRetirement = (birthYear, firstName) => {
-    const age = calcAge(birthYear)
-    const retirement = 65 - age
+    const age = calcAge(birthYear);
+    const retirement = 65 - age;
 
-    if(retirement > 0 ){
-        return retirement; // is imidiately executed and stoppping 
+    if (retirement > 0 ) {
+        console.log(`${firstName} retires in ${retirement} years`);
+        return retirement; 
     } else {
-        return -1 ;
+        console.log(`${firstName} has already retired 😎`);
+        return -1;
     }
-    console.log(`${firstName} has already retired 😎 `)
-    };
+}
 
 console.log(yearsUntillRetirement (1976, 'Cezary'));
 console.log(yearsUntillRetirement(1950, 'Mike'));

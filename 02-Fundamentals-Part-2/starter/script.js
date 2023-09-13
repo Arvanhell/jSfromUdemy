36f64a216ad1fea1977d0d820992e805d824aac2
Arvanhell <arrivald@gmail.com>
2023-09-13 19:49:18 +0100

    update of last cestion with Bonus challenge

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1141,28 +1141,7 @@ const totals = [];
                 
                 console.log(bills, tips, totals);
 
-
-
-
-
-//                With Bonus:
-
-                const calcTip = function (bill) {
-                  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-                }
-                 
-                 
-                const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
-                const tips = [];
-                const totals = [];
-                 
-                for (let i = 0; i < bills.length; i++) {
-                  const tip = calcTip(bills[i]);
-                  tips.push(tip);
-                  totals.push(tip + bills[i]);
-                }
-                 
-                console.log(bills, tips, totals);
+//                With Bonus
                  
                 const calcAverage = function (arr) {
                   let sum = 0;
@@ -1172,7 +1151,7 @@ const totals = [];
                   }
                   return sum / arr.length;
                 }
-                 
+       
                 console.log(calcAverage([2, 3, 7]));
                 console.log(calcAverage(totals));
                 console.log(calcAverage(tips));

38703afdfc85b8defcc716de144e3ef989229af8
Arvanhell <arrivald@gmail.com>
2023-09-23 22:38:47 +0100

    working within game guess mu number. Aplied another modification for DOM

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -17,6 +17,7 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 //  DOM maipulating within the secret number we added
 let score = 20;
 
+let highscore = 0;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -35,6 +36,11 @@ document.querySelector(".check").addEventListener("click", function () {
   document.querySelector('body').style.backgroundColor = '#60b347';
 
   document.querySelector('.number').style.width = '30rem';
+// new score as highscore
+  if(score > highscore){
+    highscore = score;
+    document.querySelector('.highscore').textContent = highscore;
+  }
 
 //when guess is to high
   } else if (guess > secretNumber) {

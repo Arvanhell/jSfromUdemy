530000759516223ac0ba534083d9bbcd052a8bfb
Arvanhell <arrivald@gmail.com>
2023-09-30 19:10:36 +0100

    working on running dice, implementing variables and functions to roll the dice.

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,37 +1,45 @@
 'use strict';
-//.
+// Selecting elements
+const score0El = document.querySelector('#score--0');   // DOM element
+const score1El = document.getElementById('score--1');   // DOM element
+const current0El = document.getElementById('current--0'); // DOM
+const current1El = document.getElementById('current-1');  // DOM
+const diceEl = document.querySelector('.dice');         // DOM element
+const btnNew = document.querySelector('.btn--new');     // DOM element
+const btnRoll = document.querySelector('.btn--roll');   // DOM element
+const btnHold = document.querySelector('.btn--hold');   // DOM element
 
-//Example fetch using DnD5eAPI - place subclasses in ul
-document.querySelector('button').addEventListener('click', getFetch) //1 click the button and...
 
-function getFetch(){
-  const choice = document.querySelector('input').value
-   // 2 grab the value of the input we store that in the choice variable 
-  // choice variable is in the template string $ { choice }
-  // data JSON is back if we requested proper data 
-  const url = `https://www.dnd5eapi.co/api/spells/${choice}`
+score0El.textContent = 0; // display within DOM
+score1El.textContent = 0; // display within  DOM
+diceEl.classList.add('hidden'); // display with DOM at start is HIDDEN
 
-  fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-       console.log(data.subclasses);
-       // ALWAYS CONSOLE LOG to test connections
-       //console.log(data.subclasses[0].name)
-        //console.log(data.subclasses[1].name)
-       data.subclasses.forEach (obj => {
-        //  3 we loop trough the data 
-        console.log(obj.name)
-        //  4 create an li 
-        const li = document.createElement('li')
-        //  5 ad text to li
-        li.textContent = obj.name 
-        //  6 append the li to the ul fucntion appendChild(child)
-        document.querySelector('ul').appendChild(li) 
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0; // 
+//
 
-      })
-    })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
-}
+// First Roll Dice & Rolling dice functionality
+btnRoll.addEventListener('click', function() {
+  //1. Generating a random dice roll
+  const dice = Math.trunc(Math.random()*6) + 1; // trunc is change floating for example  0.23 to 0 and adding + 1 
 
+  //2. Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src =`dice-${dice}.png`; // didplay any dice depend roll
+
+  //3. Check for rolled  1: if true, switch to next player
+    if (dice !== 1){
+        // Add to the current score
+        currentScore += dice;
+
+         // changed as displayed bellow to next 
+         //current0El.textContent = currentScore // CHANGE LATER 
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+       // changed in to displayed above
+    }else{
+        // Switch to next player
+          activePlayer = activePlayer === 0 ? 1 : 0; // working after one player get number 1 is swapping to next player
+          // reasigning the active player
+    }
+})

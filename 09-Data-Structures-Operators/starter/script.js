486c05d00513b26d1672000c2fe755604f2a5a31
Arvanhell <arrivald@gmail.com>
2023-10-18 19:37:49 +0100

    practice and training within operators

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,55 +1,57 @@
 'use strict';
 
-// Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
 
-// Data needed for first part of the section
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-openingHours: {
-  thu: {
-    open: 12,
-    close: 22,
-  },
-  fri: {
-    open: 11,
-    close: 23,
-  },
-  sat: {
-    open: 0, // Open 24 hours
-    close: 24,
-  },
-},
 
-order: function(starterIndex, mainIndex) {
-  return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  // ES6 enhanced object literals
+  openingHours,
+
+  order(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-   
-  orderDelivery: function ({starterIndex, mainIndex,time, address}) {
-    console.log(`Order received ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
-  }
-  //Order received Garlic Bread and Risotto will be delivered to Via del Sole, 21 at 22:30
+  orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
 
-  /*
-  orderPasta: function(ing1, ing2, ing3){
-    console.log(`Here is your delicious pasta with ${ing1},${ing2},${ing3}`);
-  };
-  */
+  orderPasta(ing1, ing2, ing3) {
+    console.log(
+      `Here is your declicious pasta with ${ing1}, ${ing2} and ${ing3}`
+    );
+  },
 
+  orderPizza(mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
 };
 
-restaurant.orderDelivery({
-  time: '22:30',
-  address: 'Via del Sole, 21',
-  mainIndex: 2,
-  starterIndex: 2,
-})
+
+
+
+
 
 
 const {name, openingHours, categories} = restaurant;
@@ -61,6 +63,8 @@ console.log(restaurantName, hours, tags);
 //default value
 const  {menu = [], starterMenu: starters = []} = restaurant
 console.log(menu, starters);
+
+
 
 // Mutating  variables 
 let a = 111;
@@ -77,6 +81,64 @@ const {
 console.log(o, c); 
 // 11  23
 
+
+
+                // coaelescing operator  ??
+
+
+   // restaurant.numGuests = 0;
+const guesTs = restaurant.numGuests || 10;
+console.log(guesTs); // 0c
+
+// nullish : null undefined ( NOT 0 or empty string '')
+
+const guesTsCorrect = restaurant.numGuests ?? 10;
+console.log(guesTsCorrect); // 10
+
+
+
+
+
+      console.log('---- OR ----');
+
+// Use any fata type, return any data type, short -circuting
+console.log(3 || 'Jonas'); // 3
+console.log('' || 'Jonas'); //  Jonas
+console.log(true || 0); // true
+console.log(undefined || null); // null
+
+console.log(undefined || 0 || '' || 'Hello' || 23 || null);  // Hello
+
+
+// restaurant.numGuests = 23
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1); 
+// 10 
+const guest2 = restaurant.numGuests || 10
+console.log(guest2); // 10 because at this moment restaurantGuest is not defined value,  but when we define value as restaurant.numGuests = 23; the truth value will be 23 
+
+      console.log('---- AND ----');
+
+  // And work oposite way than OR 
+
+  console.log(0 && 'Jonas'); // 0 value and answer is 0
+  console.log(7 && 'Jonas '); // truthy value but instead is answer Jonas
+
+  console.log('Hello' && 23 && Null && 'jonas');  // null in moment any value become false then all became falsy 
+
+  // practical example
+  if (restaurant.orderPizza) {
+    restaurant.orderPizza('mushroom', 'spinach');
+  }
+restaurant.orderPizza & restaurant.orderPizza 
+('mushrooms', 'spinach');
+
+
+
+
+
+//                                      
+// The Spread Operator (...)
 const arr = [7, 8, 9];
 const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
 console.log(badNewArr); 
@@ -163,5 +225,67 @@ const [x,y,z] = arr;
 console.log(x, y, z);
 console.log(arr);
 */
+
+
+//DESTRUCTURONG
+
+/// Rest Pattern and Parameters
+// SPREAD because on Right side of = 
+       const arrA= [1,2, ...[3, 4]]
+// REST because on Left side of = 
+//     const [a, b ...other] =  [1, 2, 3, 4, 5];
+//       console.log(a, b, others);
+
+
+// the rest element must be as the last element (int his manner we have ...otherFood)
+const [pizza, , risotto, ...otherFood] = [
+  ...restaurant.mainMenu, 
+  ...restaurant.starterMenu
+];
+console.log(pizza, risotto, otherFood);
+// opt Pizza Risotto
+//(4) ["Foccacia", "Bruschetta", "Garlic Bread", "Caprese Salad"]
+
+// Now try in object 
+// Object
+
+const {sat, ...weekday} = restaurant.openingHours;
+console.log(weekday);
+/*
+{thu: {…}, fri: {…}}
+fri: {open: 11, close: 23}
+thu: {open: 12, close: 22}
+[[Prototype]]: Object
+*/
+
+// FUNCTION
+
+const add = function(...numbers) { 
+  let sum = 0;
+  for (let i= 0; i < numbers.length; i++) 
+    sum += numbers[i];
+    console.log(numbers);
+    console.log(sum);
   
+};
+
+add(2, 3);
+add(5, 3, 7, 2);
+add(8,2,5,3,2,1,4);
+
+const x = [23, 5, 7];
+add(...x)
+console.log(x);
+
+/*
+   5 // sum
+   (4) [5, 3, 7, 2] // numbers[i]
+   17 // sum 
+   (7) [8, 2, 5, 3, 2, 1, 4] // numbers[i]
+   25 // sum 
+   (3) [23, 5, 7] // numbers[i]
+   35 // sum 
+   (3) [23, 5, 7] //numbers
+*/
+restaurant.orderPizza('mushrooms', 'onin', 'olives', 'spinach';)
 

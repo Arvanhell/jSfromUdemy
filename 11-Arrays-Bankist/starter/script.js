860016b292a7592e72ff1710d880723955c08e9c
Arvanhell <arrivald@gmail.com>
2023-11-01 22:55:06 +0000

    continue working on arrays using forEach method

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -1,10 +1,12 @@
 'use strict';
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// BANKIST APP
+//* ///////////////////////////////////////////////
+//* ///////////////////////////////////////////////
 
-// Data
+//* ----- BANKIST APP ----- *//
+
+//*  Data
+
 const account1 = {
   owner: 'Jonas Schmedtmann',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
@@ -35,7 +37,8 @@ const account4 = {
 
 const accounts = [account1, account2, account3, account4];
 
-// Elements
+//*  Elements
+
 const labelWelcome = document.querySelector('.welcome');
 const labelDate = document.querySelector('.date');
 const labelBalance = document.querySelector('.balance__value');
@@ -63,7 +66,8 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 //* ///////////////////////////////////////////////
 //* ///////////////////////////////////////////////
-// LECTURES
+
+//* LECTURES
 
 const currencies = new Map([
   ['USD', 'United States dollar'],
@@ -71,9 +75,11 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+//const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
 //* ///////////////////////////////////////////////
 
+/*
 let arr = ['a', 'b', 'c', 'd', 'e'];
 
 console.log(arr.slice(2)) // return new array with extracted  // ['c', 'd', 'e']
@@ -85,19 +91,77 @@ console.log(arr.slice(1, -2)); // ['b', 'c']
 console.log(arr.slice()); 
 console.log([...arr]);
 
-// SPLICE
+//* SPLICE
 
-//console.log(arr.splice(2)); // ['c','d','e']
+//*console.log(arr.splice(2)); // ['c','d','e']
 arr.splice(-1); // ['a', 'b', 'c', 'd']
 console.log(arr); // mutated and left ['a', 'b'] only
 arr.splice(1, 2);  // second element is the number of elements you want to deleted 
 
-// reverse
+//* REVERSE
  arr = ['a', 'b', 'c', 'd', 'e'];
  const arr2 = ['j', 'k', 'l', 'm', 'n'];
  console.log(arr2.reverse()) // 
- console.log(arr2); // mutated original array  ['n','m','l','k','j']
+ console.log(arr2); // mutated original array  ['n','m','l','k','j'] // 
 
-// concat methid
+//* CONCAT method
 
 const letters = arr.concat(arr2);
+console.log(letters); // ) ['a', 'b', 'c', 'd', 'e', 'n', 'm', 'l', 'k', 'j']
+
+console.log([...arr, ...arr2]); // ['a', 'b', 'c', 'd', 'e', 'n', 'm', 'l', 'k', 'j']
+
+//* JOIN 
+
+console.log(letters.join(' - ')); // a - b - c - d - e - n - m - l - k - j 
+console.log(letters.join(' * ')); // a * b * c * d * e * n * m * l * k * j
+console.log(letters.join('')); // abcdenmlkj
+
+//* ////------------ 143. The new at() Method ----------- ////
+
+const arri = [ 23, 11, 64];
+console.log(arri[0]); // 23
+console.log(arri.at(0)); // 23
+
+console.log(arri[arr.lengt - 1]); // 64 
+console.log(arri.slice(-1)[0]);  // 64
+
+console.log(arri.at(-1));  // 64
+
+//* at method work on the strings 
+console.log('cezary'.at(-1)) // y
+*/
+//* ////--------- 144. looping arrays forEach----------- ////
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+/*
+//* for (const movement of movements) {
+  for (const [i, movement] of movements.entries()) {
+  if (movement > 0 ) {
+    console.log(`You deposited ${movement}`);
+  } else {
+    console.log(`You withdraw ${Math.abs(movement)}`);
+  }
+}
+*/
+console.log('//--------FOREACH ----------//');
+
+movements.forEach(function(mov, i, arr) {
+  // first parameter second always index and the thirdone always entire array for looping over ....
+  if (mov > 0 ) {
+    console.log(`Movement ${i + 1}: You deposited ${mov}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdraw ${Math.abs(mov)}`);
+  }
+});
+//--------FOREACH ----------//
+/*
+Movement 1: You deposited 200
+Movement 2: You deposited 450
+Movement 3: You withdraw 400
+Movement 4: You deposited 3000
+Movement 5: You withdraw 650
+Movement 6: You withdraw 130
+Movement 7: You deposited 70
+Movement 8: You deposited 130
+*/

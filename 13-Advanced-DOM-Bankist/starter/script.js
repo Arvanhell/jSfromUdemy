3cae37cddc3e2fb6b383fc07e5e2e1ab1a876c76
Arvanhell <arrivald@gmail.com>
2023-11-14 23:36:51 +0000

    styles atributes and classes in DOM manioulation

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -48,8 +48,8 @@ document.addEventListener('keydown', function (e) {
 Interface between JS and browser
     //* 👉 Allows us to make JavaScript interact with the browser;
     
-    //* 👉 We can write HavaScipt to create, modify and delete HTML elements;
-       //* set styles, slasses and atributtes; and listen and respond to events;
+    //* 👉 We can write JavaScipt to create, modify and delete HTML elements;
+       //* set styles, classes and atributtes, and listen and respond to events;
     
    //* 👉 DOM tree is generated from an HTML document, which we can then interact with;
    
@@ -106,6 +106,91 @@ console.log(document.documentElement);
 console.log(document.head);
 console.log(document.body);
 
-document.querySelector('.header');
+const header = document.querySelector('.header');
 const allSections = document.querySelectorAll('.section');
 console.log(allSections);
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+console.log();
+
+console.log(document.getElementsByClassName('btn'));
+
+//* Creating and inserting elements.
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.textContent = 'We use cookies for improved functionality and analytics.';
+message.innerHTML = 
+'We use cookies for improved functionality and analytics. <button class=" btn btn--close-cookie">Cookie!</button>'; 
+
+//header.prepend(message); // top of our choosen element
+header.append(message);  //*  only on bottom our header element 
+//header.append(message.cloneNode(true))  // everywhere as belong to area 
+
+// header.before(message); // before
+// header.after(message);   // after 
+
+
+//* Delete elements
+
+document.querySelector('.btn--close-cookie').addEventListener('click', function(){
+  message.remove();
+   //message.parentElement.removeChild(message); // old school, before in use
+});
+
+//*-------->    ------------------------------------   <---------
+//*---->         187. Styles Atributes and classes          <----
+//*-------->    ------------------------------------   <---------
+
+// continue from prevous class
+
+//Style
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+console.log(message.style.color);  // no output
+console.log(message.style.backgroundColor); // rgb(55, 56, 61)
+
+console.log(getComputedStyle(message).color);  // rgb(187, 187, 187)
+console.log(getComputedStyle(message).height); // 49px
+
+message.style.height = Number.parseFloat(getComputedStyle(message).height, 10) +  30 + 'px';
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+
+//Attributes
+
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);  // Bankist logo
+console.log(logo.src);  // http://127.0.0.1:8080/img/logo.png
+console.log(logo.className);  // nav__logo
+
+//Non - standard
+console.log(logo.designer);  // won't be readed 
+console.log(logo.getAttribute('designer'));  // now is reading Jonas
+logo.setAttribute('company', 'Bankist')  // creating 
+
+console.log(logo.getAttribute('src')); // img/logo.png
+
+const link = document.querySelector('.nav__link--btn')
+console.log(link.href);   // http://127.0.0.1:8080/#
+console.log(link.getAttribute('href'));   // #
+
+// Data Attribute
+
+console.log(logo.dataset.versionNumber);  // 3.0 // setted in the logo within html
+
+//* Classes
+logo.classList.add('c', 'fake');
+logo.classList.remove('c', 'fakeclas');
+logo.classList.toggle('c', );
+logo.classList.contains('c'); // not includes when called in array
+
+// dont use it
+// logo.className = 'jonas'
+
+//*-------->    ------------------------------------   <---------
+//*---->         188. Implementing smooth scroling          <----
+//*-------->    ------------------------------------   <---------

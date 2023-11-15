2c2626ce31b97204e572da4c353ab2e87cf1524b
Arvanhell <arrivald@gmail.com>
2023-11-15 23:19:36 +0000

    event delegation  DOM manioulation implementing page navigation

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -1,12 +1,14 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
-
+//////////////////////////////////////
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+///////////////////////////////////////
+// Modal window
 
 const openModal = function (e) {
   e.preventDefault();
@@ -32,6 +34,31 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+
+btnScrollTo.addEventListener('click', function(e) {
+  const s1coords = section1.getBoundingClientRect(); 
+  console.log(s1coords);
+
+    section1.scrollIntoView({behavior: 'smooth'})
+});
+
+// * ////////////////   Page Navigation  ////////////////////////
+
+    //1. Add event listener to common parent element
+    //2. Determine what element originated the event
+document.querySelector('.nav__links')
+    .addEventListener('click', function (e){
+      e.preventDefault();  // preventing default behaviour
+              //matching strategy 
+        if (e.target.classList.contains('nav__link')){
+            const id = e.target.getAttribute('href'); 
+         document.querySelector(id).scrollIntoView({behavior: 'smooth'});
+      }
+});
+      
+
+
 
 
 //*-------->        ------------------------------------      <---------
@@ -102,6 +129,7 @@ represented by JavaScript object
 //*---->       186. Selecting, Creating, and Deleting Elements       <----
 //*-------->        ------------------------------------        <---------
 
+/*
 console.log(document.documentElement);
 console.log(document.head);
 console.log(document.body);
@@ -124,21 +152,21 @@ message.textContent = 'We use cookies for improved functionality and analytics.'
 message.innerHTML = 
 'We use cookies for improved functionality and analytics. <button class=" btn btn--close-cookie">Cookie!</button>'; 
 
-//header.prepend(message); // top of our choosen element
+//* header.prepend(message); // top of our choosen element
 header.append(message);  //*  only on bottom our header element 
-//header.append(message.cloneNode(true))  // everywhere as belong to area 
+//* header.append(message.cloneNode(true))  // everywhere as belong to area 
 
-// header.before(message); // before
-// header.after(message);   // after 
+//* header.before(message); // before
+//* header.after(message);   // after 
 
 
 //* Delete elements
 
 document.querySelector('.btn--close-cookie').addEventListener('click', function(){
   message.remove();
-   //message.parentElement.removeChild(message); // old school, before in use
+   //* message.parentElement.removeChild(message); // old school, before in use
 });
-
+*/
 //*-------->    ------------------------------------   <---------
 //*---->         187. Styles Atributes and classes          <----
 //*-------->    ------------------------------------   <---------
@@ -146,48 +174,50 @@ document.querySelector('.btn--close-cookie').addEventListener('click', function(
 // continue from prevous class
 
 //Style
-message.style.backgroundColor = '#37383d';
-message.style.width = '120%';
 
-console.log(message.style.color);  // no output
-console.log(message.style.backgroundColor); // rgb(55, 56, 61)
+//message.style.backgroundColor = '#37383d';
+//message.style.width = '120%';
 
-console.log(getComputedStyle(message).color);  // rgb(187, 187, 187)
-console.log(getComputedStyle(message).height); // 49px
+//console.log(message.style.color);  // no output
+//console.log(message.style.backgroundColor); // rgb(55, 56, 61)
 
-message.style.height = Number.parseFloat(getComputedStyle(message).height, 10) +  30 + 'px';
+//console.log(getComputedStyle(message).color);  // rgb(187, 187, 187)
+//console.log(getComputedStyle(message).height); // 49px
+
+//message.style.height = Number.parseFloat(getComputedStyle(message).height, 10) +  30 + 'px';
 
 //document.documentElement.style.setProperty('--color-primary', 'orangered');
 
 
 //Attributes
 
-const logo = document.querySelector('.nav__logo');
-console.log(logo.alt);  // Bankist logo
-console.log(logo.src);  // http://127.0.0.1:8080/img/logo.png
-console.log(logo.className);  // nav__logo
+//* const logo = document.querySelector('.nav__logo');
+//* console.log(logo.alt);  // Bankist logo
+//* console.log(logo.src);  // http://127.0.0.1:8080/img/logo.png
+//* console.log(logo.className);  // nav__logo
 
 //Non - standard
-console.log(logo.designer);  // won't be readed 
-console.log(logo.getAttribute('designer'));  // now is reading Jonas
-logo.setAttribute('company', 'Bankist')  // creating 
+//* console.log(logo.designer);  // won't be readed 
+//* console.log(logo.getAttribute('designer'));  // now is reading Jonas
+//* logo.setAttribute('company', 'Bankist')  // creating 
 
-console.log(logo.getAttribute('src')); // img/logo.png
+//* console.log(logo.getAttribute('src')); // img/logo.png
 
-const link = document.querySelector('.nav__link--btn')
-console.log(link.href);   // http://127.0.0.1:8080/#
-console.log(link.getAttribute('href'));   // #
+//* const link = document.querySelector('.nav__link--btn')
+//* console.log(link.href);   // http://127.0.0.1:8080/#
+//* console.log(link.getAttribute('href'));   // #
 
 // Data Attribute
 
-console.log(logo.dataset.versionNumber);  // 3.0 // setted in the logo within html
+//* console.log(logo.dataset.versionNumber);  // 3.0 // setted in the logo within html
 
 //* Classes
+/*
 logo.classList.add('c', 'fake');
 logo.classList.remove('c', 'fakeclas');
 logo.classList.toggle('c', );
 logo.classList.contains('c'); // not includes when called in array
-
+*/
 // dont use it
 // logo.className = 'jonas'
 
@@ -195,10 +225,10 @@ logo.classList.contains('c'); // not includes when called in array
 //*---->         188. Implementing smooth scroling          <----
 //*-------->    ------------------------------------   <---------
 
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
+// const btnScrollTo = document.querySelector('.btn--scroll-to');
+// const section1 = document.querySelector('#section--1');
 
-btnScrollTo.addEventListener('click', function(e) {
+// btnScrollTo.addEventListener('click', function(e) {
   //const s1coords = section1.getBoundingClientRect(); 
   //console.log(s1coords);
 
@@ -224,10 +254,106 @@ btnScrollTo.addEventListener('click', function(e) {
     //   behavior: 'smooth',
     // });
 
-    section1.scrollIntoView({behavior: 'smooth'})
+
+   // section1.scrollIntoView({behavior: 'smooth'})
+//});
+
+
+//*-------->    ------------------------------------   <---------
+//*---->       188. Types of events and event hadnlers      <----
+//*-------->    ------------------------------------   <---------
+/*
+
+const h1 = document.querySelector('h1');
+
+const alertH1 = function (e) {
+  alert('eddEventListener: great you are reading the heading')
+};
+
+h1.addEventListener('mouseenter',alertH1 );
+setTimeout(() =>  h1.removeEventListener('mouseenter', alertH1), 3000);
+
+/*
+//* oldschoool
+// h1.onmouseenter = function (e) { 
+//   alert('onmouseenter: great you are reading the heading');
+// }; 
+//*
+
+
+
+//*-------->    ------------------------------------   <---------
+//*---->   190. Event Propagation: Bubbling and Capturing   <----
+//*-------->    ------------------------------------   <---------
+
+
+//*-------->    ------------------------------------   <---------
+//*---->         191. Event Propagation in Practice         <----
+//*-------->    ------------------------------------   <---------
+
+// random color rgb(255,255,255)
+
+const randomInt = (min, max) => 
+  Math.floor(Math.random() * (max - min + 1 ) + min );
+const randomColor = () => `rgb(${randomInt(0,255)}, ${randomInt(0,255)}, ${randomInt(0,255)})`;
+console.log(randomColor(0, 255)); 
+
+document.querySelector('.nav__link').addEventListener('click', function (e){
+  //console.log('Link');
+  this.style.backgroundColor = randomColor();
+  console.log('Link', e.target);
+  console.log(e.currentTarget === this);
+
+  // stop propagation
+  // e.stopPropagation(); // but it is not a good idea only to use sometimes in very complex  aplications with many  handlers with the same event
+});
+/*
+document.querySelector('.nav__links').addEventListener('click', function (e){
+  this.style.backgroundColor = randomColor();
+  console.log('Container', e.target, e.currentTarget);
 });
 
+document.querySelector('.nav').addEventListener('click', function (e){
+  this.style.backgroundColor = randomColor();
+  console.log('Nav', e.target, e.currentTarget);
+});
+*/
 
 //*-------->    ------------------------------------   <---------
-//*---->       188. Types of events and event hnadlers      <----
+//*----> 192.Event Delegation: Implementing Page Navigation <----
 //*-------->    ------------------------------------   <---------
+
+       
+//* document.querySelectorAll('.nav__link') // nodelist then we loop forEach
+//* .forEach(function(el){ // acces to current element in the nodelist
+//*   el.addEventListener('click', function(e){  // acces to the event 
+//*     e.preventDefault();  // preventing default behaviour
+    
+//*     const id = this.getAttribute('href'); 
+//*     //console.log(id);
+//*     document.querySelector(id).scrollIntoView({behavior: 'smooth'});
+//*   });
+//* }); 
+
+
+//*         // event delegation is //
+
+// *    //1. Add event listener to common parent element
+//*     //2. Determine what lelement originated the event
+
+//     document.querySelector('.nav__links').addEventListener('click', function (e){
+//       e.preventDefault();  // preventing default behaviour
+//*       //console.log(e.target);
+//             //*matching strategy
+//       if (e.target.classList.contains('nav__link')){
+//             const id = e.target.getAttribute('href'); 
+//*      //console.log(id);
+//      document.querySelector(id).scrollIntoView({behavior: 'smooth'});
+//       }
+// });
+
+
+//*-------->    ------------------------------------   <---------
+//*---->                193. DOM Traversing                 <----
+//*-------->    ------------------------------------   <---------
+

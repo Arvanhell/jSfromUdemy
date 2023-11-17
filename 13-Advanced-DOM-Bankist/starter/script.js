4ab620d1e0c582b97e0ef88d9f78b759fa51256b
Arvanhell <arrivald@gmail.com>
2023-11-17 16:02:38 +0000

    building a fade animation

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -7,8 +7,15 @@ const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
-///////////////////////////////////////
-// Modal window
+const nav = document.querySelector('.nav');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+
+//* ////////////////  Modal window  ////////////////
 
 const openModal = function (e) {
   e.preventDefault();
@@ -58,30 +65,58 @@ document.querySelector('.nav__links')
 });
       
 //* /////////////  Tabbed component   //////////////////////////
+//tabs.forEach(t => t.addEventListener('click', () =>  console.log('TAB')));
+//* event delegation 
 
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
-
-// event delegation 
-tabsContainer.addEventListener('click', function (e){
+tabsContainer.addEventListener('click', function (e) {
   const clicked = e.target.closest('.operations__tab'); // traversing to closest parent element
-
+ 
         // Guard clause
-  if(!clicked) return;  // if there is no click return imediately
-
-        //Remove active classes // <--- 
+  if(!clicked) return;  // if there is no click return imediately it fix return Null
+ 
+        // Remove active classes //
+        // <--- Active Tab
   tabs.forEach(t => t.classList.remove('operations__tab--active'));
+      clicked.classList.add('operations__tab--active'); 
+        //  <--- Active content
   tabsContent.forEach(c => c.classList.remove('operations__content--active'));
-
-    // Active tab
-  clicked.classList.add('operation__tab--active');
+  
   // Activate content area 
-  document.querySelector(`.operations__content--${clicked.dataset.tab}`)
+document.querySelector(`.operations__content--${clicked.dataset.tab}`) // templet literals to manage data tab / clicked
+// Adding content to be active and visible instead of none
   .classList.add('operations__content--active');
+}); 
+
+
+//* /////////////  Menu fade animation   //////////////////////////
+
+//Event delegation  parent container for babling up for the target NAV
+nav.addEventListener('mouseover', function(e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if(el !== link) el.style.opacity = 0.5;
+    })
+    logo.style.opacity = 0.5;
+  }
 });
 
 
+nav.addEventListener('mouseout', function(e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if(el !== link) el.style.opacity = 1;
+    })
+    logo.style.opacity = 1;
+  }
+});
 
 //*-------->        ------------------------------------      <---------
 //*-------->           184. PROJECT: "Bankist" Website        <---------
@@ -351,7 +386,7 @@ document.querySelector('.nav').addEventListener('click', function (e){
 //* }); 
 
 
-//*         // event delegation is //
+//*        // event delegation is //
 
 // *    //1. Add event listener to common parent element
 //*     //2. Determine what lelement originated the event
@@ -372,6 +407,7 @@ document.querySelector('.nav').addEventListener('click', function (e){
 //*---->                193. DOM Traversing                 <----
 //*-------->    ------------------------------------   <---------
 
+/*
 // selecting h1
  
 const h1 = document.querySelector('h1');
@@ -404,7 +440,7 @@ console.log(h1.parentElement.children); // HTML collection
 [...h1.parentElement.children].forEach(function(el) {
   if(el !== h1) el.style.transform = 'scale(0.5)';
 });
-
+*/
 //*-------->    ------------------------------------   <---------
 //*---->          194. Building A tabbed component          <----
 //*-------->    ------------------------------------   <---------

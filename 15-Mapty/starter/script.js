960a986bfc16f7bd07c6768876f9ddc139b57f8a
Arvanhell <arrivald@gmail.com>
2023-12-06 22:04:11 +0000

    setting new workouts implementing workouts into array

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -2,13 +2,7 @@
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-const form = document.querySelector('.form');
-const containerWorkouts = document.querySelector('.workouts');
-const inputType = document.querySelector('.form__input--type');
-const inputDistance = document.querySelector('.form__input--distance');
-const inputDuration = document.querySelector('.form__input--duration');
-const inputCadence = document.querySelector('.form__input--cadence');
-const inputElevation = document.querySelector('.form__input--elevation');
+
 
 class Workout {
     date = new Date();
@@ -52,12 +46,23 @@ class Cycling extends Workout {
 // const run1 = new Running([39, -12], 5.2, 24, 178);
 // const cycling1 = new Cycling([39, -12], 27, 95, 523);
 // console.log(run1, cycling1);
-//* -----------------------------------
-// Application architecture 
+//* <------------------------------------------------------------ Application architecture 
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
 class App {
     #map;                   // private class
     #mapEvent               // private class
+    #workouts = [];
+
      constructor() {
+           
             this._getPosition();
         form.addEventListener('submit', this._newWorkout.bind(this)); 
         inputType.addEventListener('change', this._toggleElevationField); // toggle hidden class
@@ -101,21 +106,76 @@ class App {
     }
 
     _newWorkout(e) {
+        const validInput = (...inputs) => 
+            inputs.every(inp => Number.isFinite(inp));
+        const allPositive = (...inputs) =>
+            inputs.every( inp => inp > 0);
+
         e.preventDefault(); 
+
+        //Get data from the form
+            const type = inputType.value;
+            const distance = +inputDistance.value;
+            const duration = +inputDuration.value;
+            const {lat,lng} = this.#mapEvent.latlng;
+            let workout;
+
+        // Check if data is valid
+            
+        // If workout running, create running object
+            if (type === 'running') {
+                const cadence = +inputCadence.value;
+
+                 // Check if data is valid
+                 if (
+                                    //  !Number.isFinite(distance) || 
+                                    //  !Number.isFinite(duration) || 
+                                    //  !Number.isFinite(cadence)
+                !validInput(distance, duration, cadence) ||
+                !allPositive(distance, duration, cadence)
+                 ) 
+                 return alert('Input have to be positive numbers  📣!');
+
+                workout = new Running([lat,lng], distance, duration, cadence);
+                    
+            }
+        // If workout cycling, create cycling object
+            if (type === 'cycling') {
+                const elevation = +inputElevation.value;
+                // CHECKING VALIDATION 
+                if (
+                !validInput(distance, duration, elevation) ||
+                !allPositive(distance, duration)
+                    )
+                return alert('Input have to be positive numbers  📣!');
+
+                workout = new Cycling([lat,lng], distance, duration, elevation );
+            }
+        // Add new object to workout array
+                 this.#workouts.push(workout);
+                 console.log(workout);
+        // Render workout on map as marker
+            //* Display marker <--------------------------------------------- display marker
+             L.marker([lat, lng])    // map marker position 
+            .addTo(this.#map)
+            .bindPopup(
+               L.popup({    // methods for binding markers
+                maxWidth: 250,
+                minWidth: 100,
+                autoclose: false,
+                closeOnClick: false,
+                className: 'running-popup',
+            })
+       )
+            .setPopupContent('Workout')    // name of poping marker
+            .openPopup();
+        // Render workout on list
+
+        // Hide form + clear         
+        
         // Clear input fields
             inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
         
-            //* Display marker <--------------------------------------------- display marker
-                const {lat,lng} = this.#mapEvent.latlng
-                 L.marker([lat, lng])    // map marker position 
-                 .addTo(this.#map)
-                 .bindPopup(
-                    L.popup({    // methods for binding markers
-                    maxWidth: 250, minWidth: 100, autoclose: false, closeOnClick: false,className: 'running-popup',
-                 })
-            )
-                 .setPopupContent('Workout')    // name of poping marker
-                 .openPopup();
     }
 }
 
@@ -170,3 +230,9 @@ leafletjs.com
 //*                     <----------------------------------------------------------------->
 //*                     |          238. Managing Workout Data: Creating Classes           |
 //*                     <----------------------------------------------------------------->
+
+//*                     <----------------------------------------------------------------->
+//*                     |                     239. Creating a New Workout                 |
+//*                     <----------------------------------------------------------------->
+
+

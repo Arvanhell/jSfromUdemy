f856d6671d112b92834a84c920e3727da419afe3
Arvanhell <arrivald@gmail.com>
2023-12-11 14:32:33 +0000

    Asynchronous JavaScript, AJAX and APIs

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -12,3 +12,23 @@ const countriesContainer = document.querySelector('.countries');
 //*            <----------------------------------------------------------------->
 //*            |                        245. Section Roadmap                     |
 //*            <----------------------------------------------------------------->
+
+//*            <----------------------------------------------------------------->
+//*            |            246. Asynchronous JavaScript, AJAX and APIs          |
+//*            <----------------------------------------------------------------->
+
+
+    // const p = document.querySelector('.p');
+    // p.textContent = 'My name is Cezary';
+    // alert('text set!');
+    // p.style.color = 'red';
+
+
+    100 + 4 #100DaysOfCode 
+
+    🎲 ANKI daily
+    🎲 Code Wars daily
+    Udemy:  JavaScript Course 2023: From Zero to Expert!
+    Asynchronous JavaScript, AJAX and APIs 
+   
+    #html #css #LearningInPublic

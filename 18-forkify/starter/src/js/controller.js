417efea97ab95f43e6c130f73ecf18354ce5757e
Arvanhell <arrivald@gmail.com>
2024-01-14 18:49:45 +0000

    adding few function to forkify

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -11,6 +11,34 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+      );
+    const data = await res.json()
+    
+    if(!res.ok) throw new Error(`${data.message} (${res.status}`);
+
+    console.log(res, data);
+    let {recipe} = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image:recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients
+    };
+    console.log(recipe);
+    } catch (err) {
+    alert (err);
+  }
+};
+showRecipe()
+//console.log("Test");
 
 //*  ------------------------------------------------------
 //*                     285.Section intro
@@ -30,6 +58,11 @@ const timeout = function (s) {
 //*  ------------------------------------------------------
 //*           288. Latest Code Updates (Parcel v2 and more)
 //*  ------------------------------------------------------
+
+  // npm init
+  // npm i parcel@2 -D
+  // npm start
+  // npm instal sass
 
 //*  ------------------------------------------------------
 //*                 289. Loading Recipe from API

af8a8d39a41d7020f1d9aac9c1f51283b66d90a6
Arvanhell <arrivald@gmail.com>
2024-01-27 19:14:58 +0000

    update and add new file to toggle hide and show form within  addrecipeView.js

--- Forkify/starter/src/js/learning.js
+++ Forkify/starter/src/js/learning.js
@@ -1515,5 +1515,55 @@ User can only see own recipes, not from others
 //*-------------------------------------------------------------
                  //* Uploading a new recipe Part 1
 //*-------------------------------------------------------------
-
+// adding import into clontroller.js
 //* creating new file addRecipeView.js
+/*
+For opening and closing form window 
+
+
+'''
+import View from "./View.js";
+import icons from 'url:../../img/icons.svg';
+
+class AddRecipeView extends View {
+   _parentElement = document.querySelector('.upload');
+
+   _window = document.querySelector('.add-recipe-window')
+   _overlay = document.querySelector('.overlay')
+   _btnOpen = document.querySelector('.nav__btn--add-recipe')
+   _btnClose = document.querySelector('.btn--close-modal')
+
+
+   constructor () {
+    super();
+    this._addHandlerShowWindow();
+    this._addHandlerCloseWindow();
+   }
+
+   toggleWindow(){
+    this._overlay.classList.toggle('hidden')
+    this._window.classList.toggle('hidden')
+   }
+
+   _addHandlerShowWindow() {
+    this._btnOpen.addEventListener('click', this.toggleWindow.bind(this))
+    };
+   
+    _addHandlerCloseWindow() {
+    this._btnClose.addEventListener('click', this.toggleWindow.bind(this))
+    this._overlay.addEventListener('click', this.toggleWindow.bind(this))
+    };
+
+   _generateMarkup() {}
+  
+};
+
+export default new AddRecipeView();
+'''
+*/
+// Add handler for Upload new recipies 
+/*
+'''
+
+'''
+*/
--- Forkify/starter/src/js/views/addrecipeView.js
+++ Forkify/starter/src/js/views/addrecipeView.js
@@ -4,36 +4,43 @@ import icons from 'url:../../img/icons.svg';
 class AddRecipeView extends View {
    _parentElement = document.querySelector('.upload');
 
-  _window = document.querySelector('.add-recipe-window');
-  _overlay = document.querySelector('.overlay');
-  _btnOpen = document.querySelector('.nav__btn--add-recipe');
-  _btnClose = document.querySelector('.btn--close-modal');
-  
+   _window = document.querySelector('.add-recipe-window')
+   _overlay = document.querySelector('.overlay')
+   _btnOpen = document.querySelector('.nav__btn--add-recipe')
+   _btnClose = document.querySelector('.btn--close-modal')
 
- // add constructor method
-    constructor() {
-        super();   // only after we call super then we can use -this kayword
-        this._addHandlerShowWindow();
-        this._addHandlerHideWindow();
-    }
-    // toogle  hide/ show bind  -this keyword
-    toogleWindow() {
-        this._overlay.classList.toggle('hidden');
-        this._window.classList.toggle('hidden');
+   constructor () {
+    super();
+    this._addHandlerShowWindow();
+    this._addHandlerCloseWindow();
+   }
+
+   toggleWindow(){
+    this._overlay.classList.toggle('hidden')
+    this._window.classList.toggle('hidden')
+   }
+
+   _addHandlerShowWindow() {
+    this._btnOpen.addEventListener('click', this.toggleWindow.bind(this))
+    };
+   
+    _addHandlerCloseWindow() {
+    this._btnClose.addEventListener('click', this.toggleWindow.bind(this))
+    this._overlay.addEventListener('click', this.toggleWindow.bind(this))
+    };
+   //------
+    addHandlerUpload() {
+     this._parentElement.addEventListener('submit', function(e){
+     e.preventDefault();
+     // creating data for setting form to uplaod, contain all value we need to add
+     // we neeed to add this process in to model
+     const data = [...new FormData(this)]; 
+     console.log(data);
+     });   
     }
 
- // add handlers to listening smurfs :)
-  _addHandlerShowWindow() {
-    this._btnOpen.addEventListener('click', this.toogleWindow.bind(this));
-  }
-  
-  _addHandlerHideWindow() {
-    this._btnClose.addEventListener('click', this.toogleWindow.bind(this));
-    this._overlay.addEventListener('click', this.toggleWindow.bind(this));
- } 
    _generateMarkup() {}
-               
-  }
-
+  
+};
 
 export default new AddRecipeView();
